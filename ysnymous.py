e9a74f257f446b7b6efb520939e5087cea4d04a1
Yasin Murat Akyüz <48564989+Yasinymous@users.noreply.github.com>
2019-10-03 12:03:49 +0300

    Update ysnymous.py

--- ysnymous.py
+++ ysnymous.py
@@ -9,7 +9,7 @@ if (on_off != 1 and on_off != 2):
 
 while on_off == 1:
     number1 = float(input("ENTER NUMBER:"))
-    operation = int(input("OPERATİON" "\nAdd(1)" "\nSubtract(2)" "\nMultiply(3)" "\nDivide(4)" "\nOFF(5)" "\nENTER:"))
+    operation = int(input("OPERATİON" "\nAdd(1)" "\nSubtract(2)" "\nMultiply(3)" "\nDivide(4)" "\nNumberBase(5)" "\nOFF(6)" "\nENTER:"))
 
     if (operation == 1):
         number2 = float(input("ENTER NUMBER:"))
@@ -28,12 +28,23 @@ while on_off == 1:
         number2 = float(input("ENTER NUMBER:"))
         partition = number1/number2
         print("{}/{} = {} ".format(number1,number2,partition))
-
+    
+    elif (operation == 4):
+        number2 = float(input("ENTER NUMBER:"))
+        partition = number1/number2
+        print("{}/{} = {} ".format(number1,number2,partition))
+    
+    elif (operation == 4):
+        number2 = float(input("ENTER NUMBER:"))
+        partition = number1/number2
+        print("{}/{} = {} ".format(number1,number2,partition))
+    
     elif (operation == 5):
+        base = int(input("NUMBER BASE:"))
+        number_base = number1**base
+        print("{}^{} = {} ".format(number1,base,number_base))
+        
+    elif (operation == 6):
         break
     else:
         print("WRONG CHARACTER!!!(TRY AGAİN)")
-
-
-
-
